c4b3378e10d38636f264a0722055673f1989fb6e
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2019-07-22 22:39:02 -0300

    Escolhendo o Tamanho do Produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -3,7 +3,6 @@ import 'package:loja_virtual/data/product_data.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 
 class ProductScreen extends StatefulWidget {
-
   final ProductData product;
 
   ProductScreen(this.product);
@@ -13,15 +12,15 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
-
   final ProductData product;
+
+  String size;
 
   _ProductScreenState(this.product);
 
   @override
   Widget build(BuildContext context) {
-
-    final Color  primaryColor = Theme.of(context).primaryColor;
+    final Color primaryColor = Theme.of(context).primaryColor;
 
     return Scaffold(
       appBar: AppBar(
@@ -33,7 +32,7 @@ class _ProductScreenState extends State<ProductScreen> {
           AspectRatio(
             aspectRatio: 0.9,
             child: Carousel(
-              images: product.images.map((url){
+              images: product.images.map((url) {
                 return NetworkImage(url);
               }).toList(),
               dotSize: 4.0,
@@ -50,17 +49,53 @@ class _ProductScreenState extends State<ProductScreen> {
               children: <Widget>[
                 Text(
                   product.title,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500
-                  ),
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w500),
                 ),
                 Text(
                   "R\$ ${product.price.toStringAsFixed(2)}",
                   style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                    color: primaryColor
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                  "Tamanho",
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 34.0,
+                  child: GridView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.symmetric(vertical: 4.0),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 8.0,
+                        childAspectRatio: 0.5),
+                    children: product.sizes.map((s) {
+                      return GestureDetector(
+                        onTap: (){
+                          setState(() {
+                            size = s;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(4.0)),
+                            border: Border.all(
+                              color: s == size ? primaryColor : Colors.grey[500],
+                              width: 3.0
+                            ),
+                          ),
+                          width: 50.0,
+                          alignment: Alignment.center,
+                          child: Text(s),
+                        ),
+                      );
+                    }).toList(),
                   ),
                 )
               ],
