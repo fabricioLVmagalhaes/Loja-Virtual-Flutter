45b3a16d1b0f64be0e5cecbbcc115fe53213cf31
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2019-07-25 20:40:53 -0300

    Criando o Modelo do Carrinho

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:loja_virtual/screens/home_sceen.dart';
 
+import 'models/cart_model.dart';
 import 'models/user_model.dart';
 
 void main() => runApp(MyApp());
@@ -10,16 +11,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModel<UserModel>(
-      model: UserModel(),
-      child:  MaterialApp(
-        title: "Flutter's clothing",
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            primaryColor: Color.fromARGB(255, 4, 125, 141)
-        ),
-        debugShowCheckedModeBanner: false,
-        home: HomeScreen(),
-      ),
+        model: UserModel(),
+        child: ScopedModelDescendant(
+            builder: (context, child, model){
+              return ScopedModel<CartModel>(
+                model: CartModel(model),
+                child: MaterialApp(
+                  title: "Flutter's clothing",
+                  theme: ThemeData(
+                      primarySwatch: Colors.blue,
+                      primaryColor: Color.fromARGB(255, 4, 125, 141)),
+                  debugShowCheckedModeBanner: false,
+                  home: HomeScreen(),
+                ),
+              )
+            })
     );
   }
 }
